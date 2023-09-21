a858647f8e95d93b21d51755741a1cd241aa24a1
LEE DONG GYU <tenenger7125@gmail.com>
2023-09-21 10:49:01 +0900

    refactor: remove typeorm db import from auth and alter UserModule import

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { User } from '@/modules/user/user.entity';
-import { UserService } from '@/modules/user/user.service';
+import { UserModule } from '@/modules/user/user.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -15,10 +13,9 @@ import { AuthService } from './auth.service';
       secret: 'SECRET', //! .env 파일에서 불러와야함.
       signOptions: { expiresIn: '1d' },
     }),
-    TypeOrmModule.forFeature([User]),
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserService],
-  exports: [UserService],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -10,5 +10,6 @@ import { User } from './user.entity';
   imports: [TypeOrmModule.forFeature([User])], // db의 레포지토리를 불러오는 동작
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
