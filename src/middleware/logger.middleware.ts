5e97c81e9d95f6426dbe1b155e79492693e0dc91
LEE DONG GYU <tenenger7125@gmail.com>
2023-09-26 14:35:01 +0900

    setting: add logger console

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,11 +1,29 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('logger.... run!');
+    const {
+      headers: { host, 'user-agent': userAgent },
+      method,
+      ip,
+      cookies: _,
+    } = req;
+    const now = new Date();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const { url } = req;
+      const ellipses = new Date().getTime() - now.getTime();
+
+      this.logger.log(`${method} ${host + url} ${statusCode} ${ip} ${userAgent}`);
+      this.logger.log(`${ellipses}ms... 시간이 걸렸습니다!`);
+    });
+
     next();
   }
 }
